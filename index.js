984d84fc3c4d359e694574a52291d0fb2da45db0
kaustubh-training <kaustubh@pplwork.com>
2022-03-10 18:12:17 +0530

    minor chamges

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const importData = require('./data.json')
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
   }) 
 
 app.get('/',(req,res)=>{
