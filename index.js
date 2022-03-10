f34c36e75f248cb2b8ff4d8db5e745d637bf2cf0
kaustubh-training <kaustubh@pplwork.com>
2022-03-10 18:04:10 +0530

    changes in api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const importData = require('./data.json')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+  }) 
 
 app.get('/',(req,res)=>{
     res.send(importData)
